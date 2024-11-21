a0960d1647dba47582d0695a395bee427a615120
VIrus7-bit <italyfotura7@gmail.com>
2024-11-21 08:50:18 -0800

    Lession 5

---
+++ Lession 5 Task 1.0/Lession 5 Task 1.0.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <unordered_map>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+int main() {
+
+    using namespace std;
+    setlocale(LC_ALL, "Rus");
+    string input;
+    cout << "Введите текст: "; getline(cin, input);
+
+    std::unordered_map<char, int> freq;
+
+    for (char ch : input) {
+        freq[ch]++;
+    }
+
+    std::vector<std::pair<char, int>> freqVec(freq.begin(), freq.end());
+
+    std::sort(freqVec.begin(), freqVec.end(), [](const std::pair<char, int>& a, const std::pair<char, int>& b) {
+        return a.second > b.second;
+        });
+
+    for (const auto& pair : freqVec) {
+        std::cout << pair.first << ": " << pair.second << std::endl;
+    }
+
+    return 0;
+}
---
+++ Lession 5 Task 2.0/Lession 5 Task 2.0.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <set>
+#include <vector>
+#include <algorithm>
+
+int main() {
+
+    using namespace std;
+    setlocale(LC_ALL, "Rus");
+    int n;
+    cout << "Введите размер массива:";cin >> n;
+
+    set<int> numbers; 
+
+    for (int i = 0; i < n; i++) {
+        int num;
+        cout << "Введите значения массива " << i << " : "; cin >> num;
+        numbers.insert(num);
+    }
+    std::vector<int> uniqueNumbers(numbers.begin(), numbers.end());
+
+    // Сортируем вектор в порядке убывания
+    std::sort(uniqueNumbers.rbegin(), uniqueNumbers.rend());
+
+    // Выводим результат
+    cout << "Результат:" << endl;
+    for (const int& num : uniqueNumbers) {
+        std::cout << num << std::endl;
+    }
+    system("pause");
+    return 0;
+}
